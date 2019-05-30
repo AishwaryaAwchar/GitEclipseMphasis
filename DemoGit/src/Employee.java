4f39cf7a2ed7c0ab8803d6ecc24797ef7e70f18a
AishwaryaAwchar <aishwaryaawchar@gmail.com>
2019-05-30 16:49:26 +0530

    commit

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,26 @@
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private double empSal;
+	public int getEmpId() {
+		return empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	
+
+}
