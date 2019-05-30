0864c9595f5bd08b098325cad052e46d9f2ccf93
AishwaryaAwchar <aishwaryaawchar@gmail.com>
2019-05-30 16:54:13 +0530

    commit

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,12 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.setEmpId(101);
+		e1.setEmpName("Aishuuu");
+		e1.setEmpSal(60000);
+
+	}
+
+}
